010d7b3a59d0720f890c788c682e9d943da6bfd3
dlswns2480 <dlswns661035@gmail.com>
2024-07-13 13:08:12 +0900

    refactor : idToken 요청하는 rest 호출 코드 제거

--- adapters/out-web/src/main/kotlin/com/pokit/auth/impl/AppleApiAdapter.kt
+++ adapters/out-web/src/main/kotlin/com/pokit/auth/impl/AppleApiAdapter.kt
@@ -1,76 +1,26 @@
 package com.pokit.auth.impl
 
 import com.pokit.auth.common.dto.ApplePublicKeys
-import com.pokit.auth.common.dto.AppleIdTokenResponse
-import com.pokit.auth.common.property.AppleProperty
 import com.pokit.auth.common.support.AppleKeyGenerator
 import com.pokit.auth.common.support.AppleTokenParser
 import com.pokit.auth.port.out.AppleApiClient
 import com.pokit.user.dto.UserInfo
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.io.Decoders
-import io.jsonwebtoken.security.Keys
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
-import java.time.Instant
-import java.util.*
 
 
 @Component
 class AppleApiAdapter(
-    private val appleProperty: AppleProperty,
     private val restTemplate: RestTemplate,
     private val appleKeyGenerator: AppleKeyGenerator,
     private val appleTokenParser: AppleTokenParser
 ) : AppleApiClient{
-    override fun getUserInfo(authorizationCode: String): UserInfo {
-        val appleTokenResponse = exchangeCodeForToken(authorizationCode)
-        val identityToken = appleTokenResponse.idToken // 인가코드로 id token 추출
-        val claims = decodeAndVerifyIdToken(identityToken) // id token을 통해 사용자 정보 추출
+    override fun getUserInfo(idToken: String): UserInfo {
+        val claims = decodeAndVerifyIdToken(idToken) // id token을 통해 사용자 정보 추출
         val email = claims["email"] as String
 
         return UserInfo(email = email)
-    }
-
-    private fun exchangeCodeForToken(authorizationCode: String): AppleIdTokenResponse {
-        val clientSecret = generateClientSecret()
-        val params = mapOf(
-            "client_id" to appleProperty.clientId,
-            "client_secret" to clientSecret,
-            "code" to authorizationCode,
-            "grant_type" to "authorization_code"
-        )
-
-        val response = restTemplate.postForObject(
-            appleProperty.tokenUrl,
-            params,
-            AppleIdTokenResponse::class.java,
-        )
-
-        return response
-    }
-
-    private fun generateClientSecret(): String {
-        val now = Instant.now()
-        val exp = now.plusSeconds(15777000) // 6개월, 수정 예정
-        val claims = Jwts.claims().subject(appleProperty.clientId).build()
-        val keyBytes = Decoders.BASE64.decode(appleProperty.privateKey
-            .replace("-----BEGIN PRIVATE KEY-----", "")
-            .replace("-----END PRIVATE KEY-----", "")
-            .replace("\\s+".toRegex(), ""))
-
-        return Jwts.builder()
-            .header().add("kid", appleProperty.keyId)
-            .and()
-            .claims(claims)
-            .issuer(appleProperty.teamId)
-            .issuedAt(Date.from(now))
-            .expiration(Date.from(exp))
-            .audience().add(appleProperty.audience)
-            .and()
-            .signWith(Keys.hmacShaKeyFor(keyBytes))
-            .compact()
     }
 
     // 애플에게 공개 키 요청 후 공개키로 idToken 내 고객 정보 추출
