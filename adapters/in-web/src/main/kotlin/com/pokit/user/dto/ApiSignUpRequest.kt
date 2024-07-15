c1689e7050c86b888421348cfb2df28f15263fc6
dlswns2480 <dlswns661035@gmail.com>
2024-07-15 15:17:37 +0900

    feat : in-web 모듈 회원가입 dto

---
+++ adapters/in-web/src/main/kotlin/com/pokit/user/dto/ApiSignUpRequest.kt
@@ -0,0 +1,24 @@
+package com.pokit.user.dto
+
+import com.pokit.user.dto.request.SignUpRequest
+import com.pokit.user.model.InterestType
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class ApiSignUpRequest(
+    @NotBlank(message = "이메일은 필수값입니다.")
+    val nickName: String,
+    @Size(min = 1, message = "최소 하나 이상이어야 합니다.")
+    val interests: List<String>
+) {
+    fun toSignUpRequest(): SignUpRequest {
+        val interestTypes = interests.map {
+            InterestType.of(it)
+        }
+
+        return SignUpRequest(
+            nickName = nickName,
+            interests = interestTypes
+        )
+    }
+}
