9812c66a03d1ac4be43a1c68ad5a36f784fbd867
dlswns2480 <dlswns661035@gmail.com>
2024-07-15 15:17:50 +0900

    feat : 로그인 API 명세

--- adapters/in-web/src/main/kotlin/com/pokit/auth/AuthController.kt
+++ adapters/in-web/src/main/kotlin/com/pokit/auth/AuthController.kt
@@ -1,7 +1,10 @@
 package com.pokit.auth
 
+import com.pokit.auth.config.ErrorOperation
 import com.pokit.auth.port.`in`.AuthUseCase
 import com.pokit.token.dto.request.SignInRequest
+import com.pokit.user.exception.UserErrorCode
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,6 +17,8 @@ class AuthController(
     private val authUseCase: AuthUseCase,
 ) {
     @PostMapping("/signin")
+    @Operation(summary = "로그인 API")
+    @ErrorOperation(UserErrorCode::class)
     fun signIn(
         @RequestBody request: SignInRequest,
     ) = ResponseEntity.ok(authUseCase.signIn(request))
