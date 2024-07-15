2c71779823df9ed93631fe5ba5053e170c2ad0a9
dlswns2480 <dlswns661035@gmail.com>
2024-07-15 15:16:43 +0900

    feat : 회원가입 로직 구

--- domain/src/main/kotlin/com/pokit/user/model/InterestType.kt
+++ domain/src/main/kotlin/com/pokit/user/model/InterestType.kt
@@ -1,7 +1,18 @@
 package com.pokit.user.model
 
+import com.pokit.common.exception.ClientValidationException
+import com.pokit.user.exception.UserErrorCode
+
 enum class InterestType(
     val kor: String,
 ) {
-    SPORTS("스포츠/레저")
+    SPORTS("스포츠/레저");
+
+    companion object {
+        fun of(input: String): InterestType {
+            return InterestType.entries
+                .firstOrNull { it.kor == input }
+                ?: throw ClientValidationException(UserErrorCode.INVALID_INTEREST_TYPE)
+        }
+    }
 }
