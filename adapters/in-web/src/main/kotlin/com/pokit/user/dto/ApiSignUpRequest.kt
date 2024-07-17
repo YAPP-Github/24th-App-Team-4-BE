21c93209b7b2ecd7754f7170658ac43d05441e7f
dlswns2480 <dlswns661035@gmail.com>
2024-07-17 14:59:03 +0900

    feat : 요청 dto validation 추가

--- adapters/in-web/src/main/kotlin/com/pokit/user/dto/ApiSignUpRequest.kt
+++ adapters/in-web/src/main/kotlin/com/pokit/user/dto/ApiSignUpRequest.kt
@@ -7,8 +7,9 @@ import jakarta.validation.constraints.Size
 
 data class ApiSignUpRequest(
     @NotBlank(message = "닉네임은 필수값입니다.")
+    @Size(max = 10, message = "닉네임은 10자 이하만 가능합니다.")
     val nickName: String,
-    @Size(min = 1, message = "최소 하나 이상이어야 합니다.")
+    @Size(min = 1, max = 3, message = "최소 하나 이상, 세개 이하만 가능합니다.")
     val interests: List<String>
 ) {
     fun toSignUpRequest(): SignUpRequest {
