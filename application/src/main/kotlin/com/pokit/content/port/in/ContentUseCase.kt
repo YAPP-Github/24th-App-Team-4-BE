d9bae26b9c4a7a1012aaca63630fe752dc0db238
dlswns2480 <dlswns661035@gmail.com>
2024-07-22 23:28:51 +0900

    feat : 컨텐츠 CUD usecase 구현

--- application/src/main/kotlin/com/pokit/content/port/in/ContentUseCase.kt
+++ application/src/main/kotlin/com/pokit/content/port/in/ContentUseCase.kt
@@ -1,8 +1,16 @@
 package com.pokit.content.port.`in`
 
+import com.pokit.content.dto.ContentCommand
 import com.pokit.content.dto.response.BookMarkContentResponse
+import com.pokit.content.model.Content
 import com.pokit.user.model.User
 
 interface ContentUseCase {
     fun bookmarkContent(user: User, contentId: Long): BookMarkContentResponse
+
+    fun create(user: User, contentCommand: ContentCommand): Content
+
+    fun update(user: User, contentCommand: ContentCommand, contentId: Long): Content
+
+    fun delete(user: User, contentId: Long)
 }
--- application/src/test/kotlin/com/pokit/content/port/service/ContentServiceTest.kt
+++ application/src/test/kotlin/com/pokit/content/port/service/ContentServiceTest.kt
@@ -2,8 +2,11 @@ package com.pokit.content.port.service
 
 import com.pokit.bookmark.BookmarkFixture
 import com.pokit.bookmark.port.out.BookmarkPort
+import com.pokit.category.CategoryFixture
+import com.pokit.category.port.out.CategoryPort
 import com.pokit.common.exception.NotFoundCustomException
 import com.pokit.content.ContentFixture
+import com.pokit.content.dto.toDomain
 import com.pokit.content.port.out.ContentPort
 import com.pokit.user.UserFixture
 import io.kotest.assertions.throwables.shouldThrow
@@ -15,7 +18,8 @@ import io.mockk.mockk
 class ContentServiceTest : BehaviorSpec({
     val contentPort = mockk<ContentPort>()
     val bookmarkPort = mockk<BookmarkPort>()
-    val contentService = ContentService(contentPort, bookmarkPort)
+    val categoryPort = mockk<CategoryPort>()
+    val contentService = ContentService(contentPort, bookmarkPort, categoryPort)
 
     Given("컨텐츠에 대해 즐겨찾기 할 때") {
         val user = UserFixture.getUser()
@@ -40,6 +44,74 @@ class ContentServiceTest : BehaviorSpec({
             val response = contentService.bookmarkContent(user, requestContentId)
             Then("북마크 처리 후 컨텐츠 아이디가 반환된다.") {
                 response.contentId shouldBe requestContentId
+            }
+        }
+    }
+
+    Given("컨텐츠 관련 요청이 들어올 때") {
+        val invalidContentId = 100L
+        val content = ContentFixture.getContent()
+
+        val command = ContentFixture.getContentCommand(1L)
+        val invalidCommand = ContentFixture.getContentCommand(2L)
+
+        val user = UserFixture.getUser()
+        val category = CategoryFixture.getCategory(user.id)
+
+        every {
+            categoryPort.loadByIdAndUserId(command.categoryId, user.id)
+        } returns category
+
+        every {
+            contentPort.persist(command.toDomain())
+        } returns command.toDomain()
+
+        every {
+            contentPort.loadByUserIdAndId(user.id, content.id)
+        } returns content
+
+        every {
+            contentPort.loadByUserIdAndId(user.id, invalidContentId)
+        } returns null
+
+        every {
+            categoryPort.loadByIdAndUserId(invalidCommand.categoryId, user.id)
+        } returns null
+
+        When("저장 요청이 들어오면") {
+            val content = contentService.create(user, command)
+            Then("저장 후 저장 된 컨텐츠를 반환한다.") {
+                content.data shouldBe command.data
+                content.title shouldBe command.title
+                content.memo shouldBe command.memo
+                content.categoryId shouldBe command.categoryId
+                content.alertYn shouldBe command.alertYn
+            }
+        }
+        When("존재하지 않는 카테고리에 대한 저장 시") {
+            Then("예외가 발생한다.") {
+                shouldThrow<NotFoundCustomException> {
+                    contentService.create(user, invalidCommand)
+                }
+            }
+        }
+
+        When("수정 요청이 들어오면") {
+            val modifiedContent = contentService.update(user, command, content.id)
+            Then("수정된 컨텐츠가 반환된다.") {
+                modifiedContent.data shouldBe command.data
+                modifiedContent.title shouldBe command.title
+                modifiedContent.memo shouldBe command.memo
+                modifiedContent.categoryId shouldBe command.categoryId
+                modifiedContent.alertYn shouldBe command.alertYn
+            }
+        }
+
+        When("존재하지 않는 컨텐츠를 수정하려 하면") {
+            Then("예외가 발생한다.") {
+                shouldThrow<NotFoundCustomException> {
+                    contentService.update(user, command, invalidContentId)
+                }
             }
         }
     }
