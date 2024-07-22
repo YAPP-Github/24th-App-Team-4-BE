6fcac6326d53666983e0c96346a11ee556c591e8
dlswns2480 <dlswns661035@gmail.com>
2024-07-22 23:27:34 +0900

    feat : 컨텐츠 엔티티, 도메인 수정

--- domain/src/main/kotlin/com/pokit/content/model/Content.kt
+++ domain/src/main/kotlin/com/pokit/content/model/Content.kt
@@ -1,10 +1,21 @@
 package com.pokit.content.model
 
+import com.pokit.content.dto.ContentCommand
+
 data class Content(
-    val categoryId: Long,
-    val type: ContentType,
-    val data: String,
-    val title: String,
-    val memo: String,
-    val alertYn: String,
-)
+    val id: Long = 0L,
+    var categoryId: Long,
+    var type: ContentType = ContentType.LINK,
+    var data: String,
+    var title: String,
+    var memo: String,
+    var alertYn: String,
+) {
+    fun modify(contetCommand: ContentCommand){
+        this.categoryId = contetCommand.categoryId
+        this.data = contetCommand.data
+        this.title = contetCommand.title
+        this.memo = contetCommand.memo
+        this.alertYn = contetCommand.alertYn
+    }
+}
