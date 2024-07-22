6570ecaf4d3e9e976a24f82e22cb4c342f53ed06
dlswns2480 <dlswns661035@gmail.com>
2024-07-22 10:54:30 +0900

    feat : TestContainer 설정 수정 및 불필요 파일 제거

--- adapters/out-persistence/src/test/kotlin/com/pokit/out/persistence/common/config/TestAuditingConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.pokit.out.persistence.common.config
-
-import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-
-@TestConfiguration
-@EnableJpaAuditing
-class TestAuditingConfig
