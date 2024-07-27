c973be9528e1c14f030270f1ada5917fec860cdf
dlswns2480 <dlswns661035@gmail.com>
2024-07-28 01:12:16 +0900

    chore : OpenFeign 설정

--- adapters/out-web/build.gradle.kts
+++ adapters/out-web/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     kotlin("plugin.spring") version "1.9.24"
 }
 
+extra["springCloudVersion"] = "2023.0.3"
+
 dependencies {
     implementation(project(":application"))
     implementation(project(":domain"))
@@ -16,9 +18,19 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+    implementation("org.bouncycastle:bcpkix-jdk15on:1.69")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.1.3")
+
 }
 
 tasks {
     withType<Jar> { enabled = true }
     withType<BootJar> { enabled = false }
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+    }
 }
