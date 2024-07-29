362a3c265f872256dbeea4790efafc718877362b
dlswns2480 <dlswns661035@gmail.com>
2024-07-29 12:00:53 +0900

    feat : 에러로그 추가

--- adapters/out-web/src/main/kotlin/com/pokit/auth/common/support/AppleSecretGenerator.kt
+++ adapters/out-web/src/main/kotlin/com/pokit/auth/common/support/AppleSecretGenerator.kt
@@ -3,6 +3,7 @@ package com.pokit.auth.common.support
 import com.pokit.auth.common.property.AppleProperty
 import com.pokit.common.exception.ExternalApiException
 import com.pokit.token.exception.AuthErrorCode
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo
@@ -19,6 +20,8 @@ import java.util.*
 class AppleSecretGenerator(
     private val appleProperty: AppleProperty
 ) {
+    private val logger = KotlinLogging.logger { }
+
     fun createClientSecret(): String {
         val expirationDate = Date.from(LocalDateTime.now().plusDays(30).atZone(ZoneId.systemDefault()).toInstant())
         val jwtHeader: Map<String, Any?> = mapOf("kid" to appleProperty.keyId, "alg" to "ES256")
@@ -46,6 +49,7 @@ class AppleSecretGenerator(
             val objects = pemParser.readObject() as PrivateKeyInfo
             converter.getPrivateKey(objects)
         } catch (e: Exception) {
+            logger.warn { "AppleException: ${e.message} / $e" }
             throw ExternalApiException(AuthErrorCode.APPLE_KEY_FAILED)
         }
     }
