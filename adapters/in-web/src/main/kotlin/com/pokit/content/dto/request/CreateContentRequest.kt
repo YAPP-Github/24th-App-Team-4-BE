c2144d44dbd62d4a8317fb9d969aee802a43af2f
dlswns2480 <dlswns661035@gmail.com>
2024-08-05 11:41:40 +0900

    feat : 링크 유효성 검증

--- adapters/in-web/src/main/kotlin/com/pokit/content/dto/request/CreateContentRequest.kt
+++ adapters/in-web/src/main/kotlin/com/pokit/content/dto/request/CreateContentRequest.kt
@@ -1,10 +1,15 @@
 package com.pokit.content.dto.request
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 
 data class CreateContentRequest(
     @field:NotBlank(message = "링크는 필수값입니다.")
+    @field:Pattern(
+        regexp = "^(https?|ftp)://[^\\s/\$.?#].[^\\s]*\$",
+        message = "링크 형식이 올바르지 않습니다."
+    )
     val data: String,
     @field:NotBlank(message = "제목은 필수값입니다.")
     @field:Size(min = 1, max = 20, message = "최대 20자까지만 입력 가능합니다.")
