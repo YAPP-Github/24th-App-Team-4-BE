e090449e31a75ce520ffc727cb5a8e8f3b1aae4c
jimin3263 <dkdldsla0000@naver.com>
2024-08-10 15:33:36 +0900

    chore: 배치서버 세팅

---
+++ adapters/in-batch/build.gradle.kts
@@ -0,0 +1,27 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+    kotlin("plugin.spring") version "1.9.24"
+}
+
+dependencies {
+    // 모듈
+    implementation(project(":domain"))
+    implementation(project(":application"))
+
+    // 라이브러리
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+
+    // 테스팅
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks {
+    withType<Jar> { enabled = true }
+    withType<BootJar> { enabled = false }
+}
---
+++ entry/batch/build.gradle.kts
@@ -0,0 +1,23 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+    kotlin("plugin.spring") version "1.9.24"
+}
+
+dependencies {
+    // 모듈
+    implementation(project(":adapters:in-batch"))
+    implementation(project(":adapters:out-persistence"))
+    implementation(project(":adapters:out-web"))
+    implementation(project(":domain"))
+
+    // 라이브러리
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
+
+tasks {
+    withType<Jar> { enabled = false }
+    withType<BootJar> { enabled = true }
+}
