814825ccd9da7e5a592d34dfefc3dd673e347dd1
Jimin Lim <50178026+jimin3263@users.noreply.github.com>
2024-08-12 10:32:32 +0900

    [feat #58] 배치서버 세팅 (#83)
    * chore: 배치서버 세팅
    
    * chore: 배치 ci/cd 설정
    
    * chore: 배치 ci/cd 설정
    
    * chore: 배치 ci/cd 설정
    
    * chore: 배치 ci/cd 설정
    
    * chore: Dockerfile 포트 수정
    
    * chore: batch 서버 정보 수정
    
    * chore: batch 서버 정보 수정
    
    * chore: 수정사항 있을때 배포되도록 옵션 추가

---
+++ entry/batch/src/main/kotlin/com/pokit/batch/BatchApplication.kt
@@ -0,0 +1,14 @@
+package com.pokit.batch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+class BatchApplication
+
+fun main(args: Array<String>) {
+    System.setProperty("spring.config.name", "application-out-web, application-core, application-out-persistence")
+    runApplication<BatchApplication>(*args)
+}
