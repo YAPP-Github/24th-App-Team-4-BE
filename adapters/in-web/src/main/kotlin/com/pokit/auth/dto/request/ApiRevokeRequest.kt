3cf3f70b1adb6c98c699e3c67dc3c98a983739be
dlswns2480 <dlswns661035@gmail.com>
2024-08-28 14:42:33 +0900

    edit : 탈퇴 로직 수정

--- adapters/in-web/src/main/kotlin/com/pokit/auth/dto/request/ApiRevokeRequest.kt
+++ adapters/in-web/src/main/kotlin/com/pokit/auth/dto/request/ApiRevokeRequest.kt
@@ -2,15 +2,11 @@ package com.pokit.auth.dto.request
 
 import com.pokit.token.dto.request.RevokeRequest
 import com.pokit.token.model.AuthPlatform
-import io.swagger.v3.oas.annotations.media.Schema
 
 data class ApiRevokeRequest(
-    @Schema(description = "플랫폼에서 받은 Refresh Token")
-    val refreshToken: String,
     val authPlatform: String
 )
 
 internal fun ApiRevokeRequest.toDto() = RevokeRequest(
-    refreshToken = this.refreshToken,
     authPlatform = AuthPlatform.of(this.authPlatform)
 )
