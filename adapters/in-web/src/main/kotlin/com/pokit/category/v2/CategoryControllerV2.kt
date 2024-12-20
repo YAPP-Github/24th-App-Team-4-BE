189057015d530d5d5dabb82468f1068bba18c8e0
최인준 <dlswns661035@gmail.com>
2024-12-20 23:39:26 +0900

    feat : 포킷 목록 조회 API Ver2

--- adapters/in-web/src/main/kotlin/com/pokit/category/v2/CategoryControllerV2.kt
+++ adapters/in-web/src/main/kotlin/com/pokit/category/v2/CategoryControllerV2.kt
@@ -2,16 +2,22 @@ package com.pokit.category.v2
 
 import com.pokit.auth.config.ErrorOperation
 import com.pokit.auth.model.PrincipalUser
+import com.pokit.category.dto.CategoriesResponse
 import com.pokit.category.exception.CategoryErrorCode
 import com.pokit.category.port.`in`.CategoryUseCase
 import com.pokit.category.v1.dto.response.CategoryResponse
 import com.pokit.category.v1.dto.response.toResponse
 import com.pokit.category.v2.dto.request.CreateCategoryRequestV2
 import com.pokit.category.v2.dto.request.toDto
+import com.pokit.common.dto.SliceResponseDto
 import com.pokit.common.wrapper.ResponseWrapper.wrapOk
+import com.pokit.common.wrapper.ResponseWrapper.wrapSlice
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
@@ -34,7 +40,7 @@ class CategoryControllerV2(
             .wrapOk()
     }
 
-    @Operation(summary = "포킷 추가 API Ver2")
+    @Operation(summary = "포킷 수정 API Ver2")
     @ErrorOperation(CategoryErrorCode::class)
     @PatchMapping("/{categoryId}")
     fun updateCategory(
@@ -44,6 +50,23 @@ class CategoryControllerV2(
     ): ResponseEntity<CategoryResponse> {
         return categoryUseCase.update(request.toDto(), user.id, categoryId)
             .toResponse()
+            .wrapOk()
+    }
+
+    @Operation(summary = "포킷 목록 조회 API Ver2")
+    @GetMapping
+    fun getCategory(
+        @AuthenticationPrincipal user: PrincipalUser,
+        @PageableDefault(
+            page = 0,
+            size = 10,
+            sort = ["createdAt"],
+            direction = Sort.Direction.DESC
+        ) pageable: Pageable,
+        @RequestParam(defaultValue = "true") filterUncategorized: Boolean
+    ): ResponseEntity<SliceResponseDto<CategoriesResponse>> {
+        return categoryUseCase.getCategoriesV2(user.id, pageable, filterUncategorized)
+            .wrapSlice()
             .wrapOk()
     }
 
