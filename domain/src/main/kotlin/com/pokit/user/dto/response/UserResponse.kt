36e9281ee0162f9fb86e000f798a191f6bc23655
최인준 <dlswns661035@gmail.com>
2024-12-23 04:24:05 +0900

    feat : 초대된 유저 dto 생성

--- domain/src/main/kotlin/com/pokit/user/dto/response/UserResponse.kt
+++ domain/src/main/kotlin/com/pokit/user/dto/response/UserResponse.kt
@@ -1,15 +1,32 @@
 package com.pokit.user.dto.response
 
 import com.pokit.user.model.User
+import com.pokit.user.model.UserImage
 
-data class UserResponse (
+data class UserResponse(
     val id: Long,
     val email: String,
-    val nickname: String
+    val nickname: String,
+)
+
+data class InvitedUserResponse(
+    val userId: Long,
+    val nickname: String,
+    val profileImage: UserImage?,
 )
 
 fun User.toResponse() = UserResponse(
     id = this.id,
     email = this.email,
-    nickname = this.nickName
+    nickname = this.nickName,
 )
+
+fun List<User>.toResponse(): List<InvitedUserResponse> {
+    return this.map {
+        InvitedUserResponse(
+            userId = it.id,
+            nickname = it.nickName,
+            profileImage = it.image
+        )
+    }
+}
