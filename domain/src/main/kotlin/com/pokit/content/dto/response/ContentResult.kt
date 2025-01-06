eed605a69f27c9a848cefb5523442bb0057cb68d
최인준 <dlswns661035@gmail.com>
2025-01-06 16:24:31 +0100

    feat : 알림 필드 삭제, 키워드, 유저 닉네임 필드 추가

--- domain/src/main/kotlin/com/pokit/content/dto/response/ContentResult.kt
+++ domain/src/main/kotlin/com/pokit/content/dto/response/ContentResult.kt
@@ -3,6 +3,7 @@ package com.pokit.content.dto.response
 import com.pokit.category.model.Category
 import com.pokit.content.model.CategoryInfo
 import com.pokit.content.model.Content
+import com.pokit.user.model.User
 import java.time.LocalDateTime
 
 data class ContentResult(
@@ -11,20 +12,22 @@ data class ContentResult(
     val data: String,
     val title: String,
     val memo: String,
-    val alertYn: String,
     val createdAt: LocalDateTime,
-    val favorites: Boolean = false
+    val favorites: Boolean = false,
+    val keyword: String,
+    val userNickname: String,
 )
 
-fun Content.toGetContentResult(favorites: Boolean, category: Category): ContentResult {
+fun Content.toGetContentResult(favorites: Boolean, category: Category, user: User): ContentResult {
     return ContentResult(
         contentId = this.id,
         category = CategoryInfo(category.categoryId, category.categoryName),
         data = this.data,
         title = this.title,
         memo = this.memo,
-        alertYn = this.alertYn,
         createdAt = this.createdAt,
-        favorites = favorites
+        favorites = favorites,
+        keyword = category.keyword.kor,
+        userNickname = user.nickName
     )
 }
