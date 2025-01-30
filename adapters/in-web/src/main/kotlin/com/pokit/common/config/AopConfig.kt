4d923ae20f7741505e5a7f5d59b33d957dd1d6c5
최인준 <54973090+dlswns2480@users.noreply.github.com>
2025-01-30 16:46:33 +0900

    [feat #227] 로그 설정 (#228)
    * chore : logback -> ignore 추가
    
    * feat : AOP Configuration
    
    * feat : 요청, 응답 로그 AOP 구현
    
    * docs : LOGBACK_XML 생성 스크립트 추가

---
+++ adapters/in-web/src/main/kotlin/com/pokit/common/config/AopConfig.kt
@@ -0,0 +1,9 @@
+package com.pokit.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.EnableAspectJAutoProxy
+
+@Configuration
+@EnableAspectJAutoProxy
+class AopConfig {
+}
