6585506d53de0c13c7884f312ebbba0d2202213f
최인준 <dlswns661035@gmail.com>
2025-02-25 11:23:38 +0900

    fix : 프로필 수정 요청바디 오류 수정

--- adapters/in-web/src/main/kotlin/com/pokit/user/dto/request/UpdateProfileRequest.kt
+++ adapters/in-web/src/main/kotlin/com/pokit/user/dto/request/UpdateProfileRequest.kt
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 
 data class UpdateProfileRequest(
-    val profileImageId: Int,
+    val profileImageId: Int?,
     @field:NotBlank(message = "닉네임은 필수값입니다.")
     @field:Size(max = 10, message = "닉네임은 10자 이하만 가능합니다.")
     val nickname: String
