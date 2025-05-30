eab96e8c6ca703caee307addff5960ba2b7097fe
최인준 <dlswns661035@gmail.com>
2025-05-30 15:37:38 +0900

    feat : 누락된 키워드 추가

--- domain/src/main/kotlin/com/pokit/user/model/InterestType.kt
+++ domain/src/main/kotlin/com/pokit/user/model/InterestType.kt
@@ -7,6 +7,11 @@ enum class InterestType(
     val kor: String,
 ) {
     DEFAULT("default"),
+    MARKETING("기획/마케팅"),
+    SHOPPING("쇼핑"),
+    PLACE("장소"),
+    READING("독서"),
+    COOKING("요리/레시피"),
     SPORTS("스포츠/레저"),
     OFFICE("문구/오피스"),
     FASHION("패션"),
